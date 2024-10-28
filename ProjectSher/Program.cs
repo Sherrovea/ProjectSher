370cf316dfde944b654df573f7c7cc21902e2525
Sherida Rovea <36451730+Sherrovea@users.noreply.github.com>
2024-10-28 12:29:27 -0300

    Add project files.

---
+++ ProjectSher/Program.cs
@@ -0,0 +1,17 @@
+﻿//CrosSher Crossfit
+string mensagemDeBoasVindas = "\nOlá! Seja bem-vindo ao CrosSher!";
+
+//Essa função exibe o logo e uma mensagem de boas vindas no console
+void ExibirLogoDaMarca()
+{
+    Console.WriteLine(@"
+░█▀▀█ █▀▀█ █▀▀█ █▀▀ ░█▀▀▀█ █──█ █▀▀ █▀▀█ 
+░█─── █▄▄▀ █──█ ▀▀█ ─▀▀▀▄▄ █▀▀█ █▀▀ █▄▄▀ 
+░█▄▄█ ▀─▀▀ ▀▀▀▀ ▀▀▀ ░█▄▄▄█ ▀──▀ ▀▀▀ ▀─▀▀");
+
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+
+
+ExibirLogoDaMarca();
