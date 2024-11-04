e9f61de5cdead89404a496ddd8c5cabf0713079f
Sherida Rovea <36451730+Sherrovea@users.noreply.github.com>
2024-11-04 18:01:28 -0300

    Adição da classe Aluno

--- ProjectSher/Program.cs
+++ ProjectSher/Program.cs
@@ -1,5 +1,34 @@
 ﻿//CrosSher Crossfit
+
+using System;
+using System.Collections.Generic;
+
 string mensagemDeBoasVindas = "\nOlá! Seja bem-vindo ao CrosSher!";
+
+public class Aluno
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public decimal Altura { get; set; }
+    public decimal Peso { get; set; }
+    public string CPF { get; set; }
+    public bool EstaPagando { get; set; }
+
+    public Aluno(string nome, int idade, decimal altura, decimal peso, string cpf, bool estaPagando)
+    {
+        Nome = nome;
+        Idade = idade;  
+        Altura = altura;
+        Peso = peso;
+        CPF = cpf;
+        EstaPagando = estaPagando;
+    }
+    public override string ToString()
+    {
+        string statusPagamento = EstaPagando ? "Pagante" : "Não Pagante";
+        return $"Nome: {Nome}, Idade: {Idade}, Status: {statusPagamento}";
+    }
+}
 
 //Essa função exibe o logo e uma mensagem de boas vindas no console
 void ExibirLogoDaMarca()
@@ -14,10 +43,10 @@ void ExibirLogoDaMarca()
 //Essa função exibe a lista de opções 
 void ExibirOpcoesDoMenu()
 {
-    Console.WriteLine("\nCadastrar aluno");
-    Console.WriteLine("Lista de alunos cadastrados: ");
-    Console.WriteLine("Lista de alunos pagantes e não pagantes: ");
-    Console.WriteLine("Consultar aluno");
+    Console.WriteLine("\nDigite 1 para cadastrar um novo aluno");
+    Console.WriteLine("Digite 2 para abrir a lista de alunos cadastros");
+    Console.WriteLine("Digite 3 para visualizar a lista de alunos pagantes e não pagantes");
+    Console.WriteLine("Digite 4 para consultar um aluno");
     Console.WriteLine("Digite Esc para sair");
 }
 
