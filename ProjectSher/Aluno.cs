e1ea88b255608749392f6987851c965cd90fe225
Sherida Rovea <36451730+Sherrovea@users.noreply.github.com>
2024-11-17 17:10:21 -0300

    Separar classe "Aluno" do program

---
+++ ProjectSher/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSher
+{
+public class Aluno
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public decimal Altura { get; set; }
+    public decimal Peso { get; set; }
+    public string CPF { get; set; }
+    public bool EstaPagando { get; set; }
+
+    public Aluno(string nome, int idade, decimal altura, decimal peso, string cpf, bool estaPagando)
+    {
+        Nome = nome;
+        Idade = idade;
+        Altura = altura;
+        Peso = peso;
+        CPF = cpf;
+        EstaPagando = estaPagando;
+    }
+    public override string ToString()
+    {
+        string statusPagamento = EstaPagando ? "Pagante" : "Não Pagante";
+        return $"Nome: {Nome}, Idade: {Idade}, Altura {Altura}, Peso {Peso}, CPF {CPF}, Status: {statusPagamento}";
+    }
+}
+
+}
--- ProjectSher/Program.cs
+++ ProjectSher/Program.cs
@@ -5,30 +5,6 @@ using System.Collections.Generic;
 
 string mensagemDeBoasVindas = "\nOlá! Seja bem-vindo ao CrosSher!";
 
-public class Aluno
-{
-    public string Nome { get; set; }
-    public int Idade { get; set; }
-    public decimal Altura { get; set; }
-    public decimal Peso { get; set; }
-    public string CPF { get; set; }
-    public bool EstaPagando { get; set; }
-
-    public Aluno(string nome, int idade, decimal altura, decimal peso, string cpf, bool estaPagando)
-    {
-        Nome = nome;
-        Idade = idade;  
-        Altura = altura;
-        Peso = peso;
-        CPF = cpf;
-        EstaPagando = estaPagando;
-    }
-    public override string ToString()
-    {
-        string statusPagamento = EstaPagando ? "Pagante" : "Não Pagante";
-        return $"Nome: {Nome}, Idade: {Idade}, Status: {statusPagamento}";
-    }
-}
 
 //Essa função exibe o logo e uma mensagem de boas vindas no console
 void ExibirLogoDaMarca()
@@ -39,6 +15,7 @@ void ExibirLogoDaMarca()
 ░█▄▄█ ▀─▀▀ ▀▀▀▀ ▀▀▀ ░█▄▄▄█ ▀──▀ ▀▀▀ ▀─▀▀");
 
     Console.WriteLine(mensagemDeBoasVindas);
+    
 }
 //Essa função exibe a lista de opções 
 void ExibirOpcoesDoMenu()
