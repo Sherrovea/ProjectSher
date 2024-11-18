420d84e2af7ab34078a16441992f0809117998a1
Sherida Rovea <36451730+Sherrovea@users.noreply.github.com>
2024-11-18 16:31:30 -0300

    Ulitma atualização

--- ProjectSher/Program.cs
+++ ProjectSher/Program.cs
@@ -1,9 +1,9 @@
 ﻿//CrosSher Crossfit
 
+using ProjectSher;
 using System;
 using System.Collections.Generic;
 
-string mensagemDeBoasVindas = "\nOlá! Seja bem-vindo ao CrosSher!";
 
 
 //Essa função exibe o logo e uma mensagem de boas vindas no console
@@ -26,13 +26,73 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 4 para consultar um aluno");
     Console.WriteLine("Digite Esc para sair");
 }
-
+//Função para cadastrar alunos
 void CadastrarAlunos()
     {
-    Console.WriteLine();
+    Console.WriteLine("/nCadastro de Aluno");
+
+    Console.WriteLine("Nome do Aluno: ");
+    string nome = Console.ReadLine()!;
+
+    Console.Write("Idade do Aluno: ");
+    int idade = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Altura do Aluno: ");
+    decimal altura = decimal.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Peso do Aluno: ");
+    decimal peso = decimal.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("CPF do Aluno: ");
+    string cpf = Console.ReadLine()!;    
+
+    Console.WriteLine("O Aluno é pagante? (s/n): ");
+    bool estaPagando = (Console.ReadLine()!.ToLower() == "s");
+    
+    Aluno novoAluno =  new Aluno(nome, idade, altura, peso, cpf, estaPagando);
+    listaDeAlunos.Add(novoAluno);
+
+    Console.WriteLine("/nAluno {nome} cadastrado com sucesso!/n");
     }
 
+void ListarAlunos()
+{
+    Console.WriteLine("/nLista de alunos cadastrados: ");
+
+    if (listaDeAlunos.Count == 0)
+    {
+        Console.WriteLine("Nenhum aluno cadastrado.");
+    }
+    else
+    {
+        foreach (var aluno in listaDeAlunos)
+        {
+            Console.WriteLine(aluno);
+        }
+
+    }
+    ExibirLogoDaMarca();
+    while (true)
+    {
+        ConsoleKeyInfo tecla = Console.ReadKey();
+
+        if(tecla.Key == ConsoleKey.Escape)
+        {
+            break;
+        }
+        else if(tecla.KeyChar == '1')
+        {
+            CadastrarAlunos();
+        }
+        else if (tecla.KeyChar == '2')
+    {
+            ListarAlunos();
+        }
+    }
+
+    ListarAlunos();
 CadastrarAlunos();
 ExibirLogoDaMarca();
 ExibirOpcoesDoMenu();
+}
 
